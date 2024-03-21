e1b83e034408ab66724b0aa57e3ef04ea4bb41d5
tbL3 <112128403+tbL3@users.noreply.github.com>
2024-03-21 16:40:29 +0100

    Update tp_cyber.sql

--- tp_cyber.sql
+++ tp_cyber.sql
@@ -1,9 +1,14 @@
-CREATE TABLE visits_counter (
+CREATE DATABASE IF NOT EXISTS tp_cyber;
+
+USE tp_cyber;
+
+CREATE TABLE IF NOT EXISTS visits_counter (
     id INT AUTO_INCREMENT PRIMARY KEY,
     visits INT NOT NULL DEFAULT 0
 );
 
-CREATE TABLE tricked_counter (
+CREATE TABLE IF NOT EXISTS tricked_counter (
     id INT AUTO_INCREMENT PRIMARY KEY,
     tricked INT NOT NULL DEFAULT 0
 );
+
